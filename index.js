2553ecb0c7cea0c0dc0fd2d6ab5c72c034803a02
kulikp1 <pashakylik2@gmail.com>
2025-02-03 19:52:19 +0200

    train middleware

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log({ Metgod: req.method });
+    console.log({ URL: req.url });
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
