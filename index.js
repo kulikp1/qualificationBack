2990e1f419482eec05036d4d533a0d7f7dedbfc1
kulikp1 <pashakylik2@gmail.com>
2025-02-04 13:34:48 +0200

    add Handle application error

--- index.js
+++ index.js
@@ -14,8 +14,16 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
+//Handle 404
 app.use((req, res, next) => {
     res.status(404).send({ status: 404, message: "Route not found" });
+});
+
+//Handle application error
+app.use((error, req, res, next) => {
+    console.error(error);
+
+    res.status(500).send({ status: 500, message: "Internal Server Error" })
 });
 
 app.listen(8080, () => {
