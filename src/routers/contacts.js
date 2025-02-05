6e34ace09c2fe8940eb56ad4e5a93d66bb7b9098
kulikp1 <pashakylik2@gmail.com>
2025-02-05 15:13:12 +0200

    add new routes

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,3 +1,32 @@
 import { Router } from 'express';
 
+ Router.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+Router.get('/contacts/:contactId', async (req, res, next) => {
+    const contactId = req.params.contactId;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+
 export default router;
--- src/server.js
+++ src/server.js
@@ -26,34 +26,9 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+ 
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const contactId = req.params.contactId;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
+  
   app.use('*', (req, res) => {
     res.status(404).json({
       message: 'Not found',
