63ee9532977116b67e876e3d7bf5b12d30613fc7
kulikp1 <pashakylik2@gmail.com>
2025-02-08 17:58:17 +0200

    add user login schema

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import rootRouter from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -12,6 +13,7 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cookieParser());
 
   app.use(
     pino({
@@ -29,7 +31,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter);
+  app.use(rootRouter);
 
   app.use('*', notFoundHandler);
 
