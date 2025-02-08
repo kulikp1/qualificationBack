6dc4a936262316391a3b1f36fd857dc4c2624c4b
kulikp1 <pashakylik2@gmail.com>
2025-02-08 18:18:07 +0200

    Update userRegistrationSchema.js

--- src/validation/userRegistrationSchema.js
+++ src/validation/userRegistrationSchema.js
@@ -1,6 +1,7 @@
-import Joi from 'joi';
+import Joi from "joi";
 
-export const userLoginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+export const userRegistrationSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
 });
