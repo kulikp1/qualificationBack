bfd510a50d2ff68e6f7ae52f2010e3d143dd8266
kulikp1 <pashakylik2@gmail.com>
2025-02-17 00:05:48 +0200

    update index.js for reset pwd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
+import { TEMP_DIR } from './constants/index.js';
+import { createDirIfDoesNotExist } from './utils/createDirIfDoesNotExist.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createDirIfDoesNotExist(TEMP_DIR);
   setupServer();
 };
 
