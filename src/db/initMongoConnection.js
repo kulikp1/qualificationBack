c0d27963160f65e1080b23016823802ae1ef8df9
kulikp1 <pashakylik2@gmail.com>
2025-02-18 19:35:14 +0200

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 import { env } from '../utils/env.js';
 
-export const initMongoConnection = async () => {
+export const initMongoDB = async () => {
   try {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
@@ -13,8 +13,8 @@ export const initMongoConnection = async () => {
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
-  } catch (err) {
-    console.log('Mongo connection was not established', err);
-    throw err;
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
 };
