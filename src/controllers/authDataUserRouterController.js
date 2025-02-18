b22bfd5bf129c3ea25c33ee3b745eac328ef240c
kulikp1 <pashakylik2@gmail.com>
2025-02-18 19:30:57 +0200

    create authDataUserRouterController

---
+++ src/controllers/authDataUserRouterController.js
@@ -0,0 +1,9 @@
+export const authDataUserRouterController = (req, res, next) => {
+  const user = req.authUser;
+
+  res.status(200).json({
+    status: 200,
+    message: 'User data retrieved successfully',
+    data: user,
+  });
+};
