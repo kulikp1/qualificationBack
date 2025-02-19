5c7b44836fa1f4d6c0cbe451c0f576bb66aa57c1
kulikp1 <pashakylik2@gmail.com>
2025-02-19 18:04:07 +0200

    Update validateBody.js

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,11 +2,13 @@ import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body, { abortEarly: false });
+    await schema.validateAsync(req.body, {
+      abortEarly: false,
+    });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      error: err.details,
+    const error = createHttpError(400, 'invalidBody: Bad Request', {
+      errors: err.details,
     });
     next(error);
   }
