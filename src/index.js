84889b924c3e9ca5c1e6465c2afd5854c51a95af
kulikp1 <pashakylik2@gmail.com>
2025-02-22 00:06:44 +0200

    update index and server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import { setupServer } from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { TEMP_DIR } from './constants/index.js';
-import { createDirIfDoesNotExist } from './utils/createDirIfDoesNotExist.js';
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
 
 const bootstrap = async () => {
-  await initMongoConnection();
-  await createDirIfDoesNotExist(TEMP_DIR);
-  setupServer();
+  await initMongoDB();
+  startServer();
 };
 
 bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { env } from './utils/env.js';
+import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import rootRouter from './routers/index.js';
-import cookieParser from 'cookie-parser';
+import { APP_CONFIG } from './constants/app.js';
+import router from './routers/router.js';
 
-const PORT = Number(env('PORT', '3000'));
+const PORT = APP_CONFIG.PORT;
 
-export const setupServer = () => {
+export const startServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cookieParser());
+
+  const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+      'https://team-03-project-front.vercel.app',
+    ],
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
+
+  app.options('*', cors(corsOptions));
 
   app.use(
     pino({
@@ -23,17 +35,14 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(cors());
-
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello! Welcome to ContactsApp!',
+      message: 'Hello world!',
     });
   });
+  app.use(router);
 
-  app.use(rootRouter);
-
-  app.use('*', notFoundHandler);
+  app.use(notFoundHandler);
 
   app.use(errorHandler);
 
