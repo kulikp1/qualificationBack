d8e07c3224eecf0ce42737b7f7651faeea583bdb
kulikp1 <pashakylik2@gmail.com>
2025-02-22 00:04:08 +0200

    clean validation folder

--- src/validation/userRegistrationSchema.js
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const userRegistrationSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-});
